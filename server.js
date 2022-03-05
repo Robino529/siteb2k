b28cdb07311c0f2588d80f762a5cecfc96fd0eaf
timoreo <timo.oreo34@gmail.com>
2022-03-05 19:27:53 +0100

    Fix pour plus tard

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ const path = require('path');
 const hostname = 'localhost';
 const port = 3000;
 const secret = fs.readFileSync("secret.txt", "utf8").trim();
+const reps = JSON.parse(fs.readFileSync("replacements.json", "utf8"));
+function* entries(obj) {
+    for (let key of Object.keys(obj)) {
+        yield [key, obj[key]];
+    }
+}
+function replaceStuff() {
+    const files = fs.readdirSync("/root/pages/");
+    files.forEach(function (file) {
+        if(file.endsWith(".html")){
+            let result = fs.readFileSync(path.join("/root/pages/", file), 'utf8');
+            for (let [key, value] of entries(reps)) {
+                result = result.replace(key, value);
+            }
+            const name = path.basename(file);
+            fs.writeFileSync(path.join("/var/www/play.timoreo.fr/html/", name), result, 'utf8');
+        }else{
+            const name = path.basename(file);
+            fs.copyFileSync(path.join("/root/pages/",file),path.join("/var/www/play.timoreo.fr/html/",name));
+        }
+    });
+}
+
 const server = http.createServer((req, res) => {
     if(req.method === "POST" && req.url === "/app/setSeason"){
         res.setHeader('Content-Type', 'text/plain');
@@ -13,18 +36,9 @@ const server = http.createServer((req, res) => {
                 body += data;
             });
             req.on('end', function() {
-                const files = fs.readdirSync("/root/pages/");
-                files.forEach(function (file) {
-                        if(file.endsWith(".html")){
-                            const data = fs.readFileSync(path.join("/root/pages/",file), 'utf8');
-                            const result = data.replace(/NOM_SAISON/g, body);
-                            const name = path.basename(file)
-                            fs.writeFileSync(path.join("/var/www/play.timoreo.fr/html/", name), result, 'utf8');
-                        }else{
-                            const name = path.basename(file);
-                            fs.copyFileSync(path.join("/root/pages/",file),path.join("/var/www/play.timoreo.fr/html/",name));
-                        }
-                });
+                reps["NOM_SAISON"] = body;
+                fs.writeFileSync("replacements.json", JSON.stringify(reps),"utf8");
+                replaceStuff();
                 res.statusCode = 200;
                 res.end("Success\n");
             });
